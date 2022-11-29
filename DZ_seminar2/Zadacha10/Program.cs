476caf18ee8fff098a0bbc14827e10f4f8bbd711
Lyudmila <lyudmila.kostornova@mail.ru>
2022-11-29 12:04:10 +0300

    Домашние задания 2-го семинара

---
+++ DZ_seminar2/Zadacha10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трёхзначное число");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+while (num < 100 || num > 1000)
+{
+    Console.WriteLine("Число не трехзначное, повторите ввод");
+    num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+int d1 = num / 100;
+int result = (num - d1 * 100) / 10;
+Console.WriteLine($"Вторая цифра числа: {result}");
---
+++ DZ_seminar2/Zadacha13V3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("Введите число");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (num > 99) {   int r = 1; int c = num / 10;
+    while (c != 0)
+    {  r++; c = c / 10;  } 
+    num = num / Convert.ToInt32(Math.Pow(10, r - 3));
+    Console.WriteLine($"Третья цифра: {num % 10}"); }
+else Console.WriteLine("Третьей цифры нет");
